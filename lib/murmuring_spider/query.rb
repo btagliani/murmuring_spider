0933819fed6bfc66daed7c6b4b895251f17e02a3
tomykaira <tomykaira@gmail.com>
2012-03-16 23:18:55 +0900

    Change definition of classes

--- lib/murmuring_spider/query.rb
+++ lib/murmuring_spider/query.rb
@@ -5,53 +5,55 @@ require 'twitter'
 #
 # Query: represents request to Twitter
 #
-class MurmuringSpider::Query
-  include DataMapper::Resource
+module MurmuringSpider
+  class Query
+    include DataMapper::Resource
 
-  property :id, Serial
-  property :type, String
-  property :target, String, :unique => :type
-  property :opts, Object
+    property :id, Serial
+    property :type, String
+    property :target, String, :unique => :type
+    property :opts, Object
 
-  self.raise_on_save_failure = true
+    self.raise_on_save_failure = true
 
-  class << self
-    #
-    # Add an query
-    # * _type_ : request type.  Name of a Twitter's method
-    # * _target_ : First argument of the Twitter's method.  Usually, an user or a query
-    # * _opts_ : options. Second argument of the Twitter's method.
-    #
-    # returns : created Query instance
-    #
-    # raises : DataMapper::SaveFailureError
-    #
-    def add(type, target, opts = {})
-      create(:type => type, :target => target, :opts => opts)
+    class << self
+      #
+      # Add an query
+      # * _type_ : request type.  Name of a Twitter's method
+      # * _target_ : First argument of the Twitter's method.  Usually, an user or a query
+      # * _opts_ : options. Second argument of the Twitter's method.
+      #
+      # returns : created Query instance
+      #
+      # raises : DataMapper::SaveFailureError
+      #
+      def add(type, target, opts = {})
+        create(:type => type, :target => target, :opts => opts)
+      end
     end
-  end
 
-  #
-  # Execute Twitter request and update :since_id of _opts_
-  # This method has side effect
-  #
-  # returns : Array of Twitter::Status
-  #
-  def collect_statuses
-    res = Twitter.__send__(type, target, opts)
-    unless res.empty?
-      self.opts = opts.merge(:since_id => res.first.id)
-      save
+    #
+    # Execute Twitter request and update :since_id of _opts_
+    # This method has side effect
+    #
+    # returns : Array of Twitter::Status
+    #
+    def collect_statuses
+      res = Twitter.__send__(type, target, opts)
+      unless res.empty?
+        self.opts = opts.merge(:since_id => res.first.id)
+        save
+      end
+      res
     end
-    res
-  end
 
-  #
-  # Collect tweet statuses and save them
-  #
-  # returns : Array of MurmuringSpider::Status
-  #
-  def run
-    collect_statuses.map { |s| MurmuringSpider::Status.create(s) }
+    #
+    # Collect tweet statuses and save them
+    #
+    # returns : Array of MurmuringSpider::Status
+    #
+    def run
+      collect_statuses.map { |s| Status.create(s) }
+    end
   end
 end
--- lib/murmuring_spider/status.rb
+++ lib/murmuring_spider/status.rb
@@ -1,17 +1,19 @@
 require 'dm-core'
 require 'twitter'
 
-class MurmuringSpider::Status
-  include DataMapper::Resource
-  property :id, Serial
-  property :tweet_id, String
-  property :text, String, :length => 255
-  property :user_id, String
-  property :screen_name, String
-  property :created_at, DateTime
+module MurmuringSpider
+  class Status
+    include DataMapper::Resource
+    property :id, Serial
+    property :tweet_id, String
+    property :text, String, :length => 255
+    property :user_id, String
+    property :screen_name, String
+    property :created_at, DateTime
 
-  def initialize(s)
-    super(:tweet_id => s.id, :text => s.text, :user_id => s.user.id,
-    :screen_name => s.user.screen_name, :created_at => s.created_at)
+    def initialize(s)
+      super(:tweet_id => s.id, :text => s.text, :user_id => s.user.id,
+      :screen_name => s.user.screen_name, :created_at => s.created_at)
+    end
   end
 end
